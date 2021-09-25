a45c78ceb040560f8cef51a0686b5c638b9b6c90
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-25 14:44:01 +1000

    wrong db name

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ const { Database } = require("@forbidden_duck/super-mongo");
 const collections = require("./collections");
 const schemas = require("./schemas");
 
-module.exports = new Database("mongo-web-app", [
+module.exports = new Database("mongowebapp", [
     collections.ips,
     collections.refresh_tokens,
     collections.users,
