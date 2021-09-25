904dae5e7f4ed85ec0cfffef631068f3ce1b9484
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-25 14:50:56 +1000

    implement IP logging

--- backend/loaders/routes.js
+++ backend/loaders/routes.js
@@ -6,6 +6,25 @@ const path = require("path");
  * @param {import("./mongodb").MongoType} Mongo
  */
 module.exports = (app, Mongo) => {
+    app.use(async (req, res, next) => {
+        // Remove PORT from ip
+        const address = req.ip.replace(/((?::))(?:[0-9]+)$/, "");
+        const ipDocument = await Mongo.services.IPService.find({ address });
+        if (ipDocument && ipDocument._id) {
+            await Mongo.services.IPService.update(
+                {
+                    $inc: {
+                        count: 1,
+                    },
+                },
+                { _id: ipDocument._id }
+            );
+        } else {
+            await Mongo.services.IPService.create({ address, count: 1 });
+        }
+        next();
+    });
+
     // TODO Routes
     app.use((req, res, next) => {
         if (process.env.NODE_ENV === "production") {
