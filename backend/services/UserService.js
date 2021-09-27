30efe25368e063b2e499e827f7e02e228fd54d65
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-27 15:35:00 +1000

    modifiedAt is supposed to be null

--- backend/services/UserService.js
+++ backend/services/UserService.js
@@ -39,7 +39,7 @@ module.exports = class UserService {
     async create(data) {
         data._id = SuperUtils.ID.create("SHA256");
         data.createdAt = new Date();
-        data.modifiedAt = new Date();
+        data.modifiedAt = null;
 
         try {
             await UserCollection.insertOne(data);
