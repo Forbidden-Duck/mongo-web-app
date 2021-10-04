3e8bf142c779d026cff61c55cd9b8e9f9b7fba46
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-04 13:24:19 +1000

    Implement the mui theme provider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,9 +2,15 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 console.log("Hello, welcome to the console...");
+
+const theme = createTheme({});
 ReactDOM.render(
     <StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </StrictMode>
 );
