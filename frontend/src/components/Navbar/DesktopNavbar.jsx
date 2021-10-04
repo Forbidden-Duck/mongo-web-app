c72867d53cab49bb9c21a2ac1ac8f6bbfe6b05fb
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-04 14:54:34 +1000

    DesktopNavbar with MongoDB logo

--- frontend/src/components/Navbar/DesktopNavbar.jsx
+++ frontend/src/components/Navbar/DesktopNavbar.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./NavbarStyles.css";
 
-function DesktopNavbar() {}
+import { AppBar, Toolbar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+function DesktopNavbar() {
+    const classes = makeStyles((theme) => ({
+        menuButton: {
+            marginRight: theme.spacing(2),
+            display: "flex",
+            alignItems: "center",
+        },
+    }))();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className="navbar-header">
+                <div className="navbar-header-items">
+                    <div className="navbar-logo" />
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+}
 
 export default DesktopNavbar;
--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-function Home() {}
+function Home() {
+    return <p>Temporary test</p>;
+}
 
 export default Home;
