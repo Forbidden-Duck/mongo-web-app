716e2420a275838909ae717dabdaf32663cb21e0
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-04 13:07:27 +1000

    Use newly created components and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,27 @@ import {
     Switch,
 } from "react-router-dom";
 
+// Components
+import DesktopNavbar from "./components/Navbar/DesktopNavbar";
+import MobileNavbar from "./components/Navbar/MobileNavbar";
+
+// Routes
+import Home from "./routes/Home/Home";
+
+import { useMediaQuery } from "@mui/material";
+
 function App() {
+    const isMobile640 = useMediaQuery("(max-width:640px)");
+
     return (
         <div>
             <Router>
-                <Switch></Switch>
+                {isMobile640 ? <MobileNavbar /> : <DesktopNavbar />}
+                <Switch>
+                    <Route exact path="/" component={Home} />
+
+                    <Redirect from="*" to="/" />
+                </Switch>
             </Router>
         </div>
     );
