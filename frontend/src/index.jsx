c63f28bd757359996f9c1e8eab8e45aa17647691
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-04 13:40:49 +1000

    React redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,15 +2,22 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import reducer from "./store";
+
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 console.log("Hello, welcome to the console...");
 
+const store = configureStore({ reducer: reducer });
 const theme = createTheme({});
 ReactDOM.render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </Provider>
     </StrictMode>
 );
---
+++ frontend/src/store/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+// TODO Reducers
+
+export default combineReducers();
