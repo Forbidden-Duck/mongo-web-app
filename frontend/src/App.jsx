59675014b9c1280a5f3b6691d1255764187f2d12
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-04 13:00:35 +1000

    Index and App jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom";
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Switch></Switch>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/index.jsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+console.log("Hello, welcome to the console...");
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
