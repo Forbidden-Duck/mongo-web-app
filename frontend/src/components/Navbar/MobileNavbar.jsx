ef59d0aeb6b4732cee0263631465ec0735b2f13e
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-05 14:50:44 +1000

    MobileNavbar

--- frontend/src/components/Navbar/MobileNavbar.jsx
+++ frontend/src/components/Navbar/MobileNavbar.jsx
@@ -1,5 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    AppBar,
+    IconButton,
+    Toolbar,
+    Typography,
+    Drawer,
+    MenuItem,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faUserCircle,
+    faCog,
+    faUserPlus,
+    faSignInAlt,
+    faSignOutAlt,
+    faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import { logoutUser } from "../../store/auth/Auth.actions";
 
-function MobileNavbar() {}
+function MobileNavbar() {
+    const classes = makeStyles((theme) => ({
+        menuButton: {
+            margin: theme.spacing(0.7),
+        },
+        toolbar: {
+            justifyContent: "space-between",
+            minHeight: 68,
+            background: "#fff",
+            color: "#2f2f2f",
+        },
+        buttonHover: {
+            "&:hover": {
+                color: "#8f8f8f !important",
+            },
+        },
+        navLogo: {
+            backgroundImage:
+                "url(https://webassets.mongodb.com/_com_assets/cms/MongoDB_Logo_FullColorBlack_RGB-4td3yuxzjs.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            width: "222px",
+            height: "60px",
+            "&:hover": {
+                cursor: "pointer",
+            },
+        },
+        clearText: {
+            color: "inherit",
+            textDecoration: "none",
+        },
+        textPadding: {
+            "& *:not(:last-child)": {
+                paddingRight: "20px",
+            },
+        },
+    }))();
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const { isAuthenticated } = useSelector((state) => state.auth);
+
+    // Drawer
+    const [drawer, setDrawer] = useState(false);
+    const handleDrawer = (value) => setDrawer(value ? !!value : !drawer);
+    const handleDrawerLogout = async () => {
+        handleDrawer(false);
+        await dispatch(logoutUser());
+        history.push("/");
+    };
+    const handleLogoClick = () => history.replace("/");
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <div className={classes.navLogo} onClick={handleLogoClick} />
+                <div className={classes.menuButton}>
+                    <IconButton
+                        aria-controls="site-select"
+                        aria-haspopup="true"
+                        color="inherit"
+                        onClick={handleDrawer}
+                    >
+                        <FontAwesomeIcon icon={faBars} />
+                    </IconButton>
+                    <Drawer
+                        anchor="right"
+                        open={drawer}
+                        onClose={() => handleDrawer(false)}
+                    >
+                        <div className={classes.menuButton}>
+                            {!isAuthenticated ? (
+                                <>
+                                    <MenuItem
+                                        onClick={() => handleDrawer(false)}
+                                        component={Link}
+                                        to="/login"
+                                    >
+                                        <Typography>
+                                            <FontAwesomeIcon
+                                                icon={faSignInAlt}
+                                            />
+                                            &nbsp; Login
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => handleDrawer(false)}
+                                        component={Link}
+                                        to="/register"
+                                    >
+                                        <Typography>
+                                            <FontAwesomeIcon
+                                                icon={faUserPlus}
+                                            />
+                                            &nbsp; Register
+                                        </Typography>
+                                    </MenuItem>
+                                </>
+                            ) : (
+                                <>
+                                    <MenuItem
+                                        onClick={() => handleDrawer(false)}
+                                        component={Link}
+                                        to="/settings"
+                                    >
+                                        <Typography>
+                                            <FontAwesomeIcon icon={faCog} />
+                                            &nbsp; Settings
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleDrawerLogout}>
+                                        <Typography>
+                                            <FontAwesomeIcon
+                                                icon={faSignOutAlt}
+                                            />
+                                            &nbsp; Logout
+                                        </Typography>
+                                    </MenuItem>
+                                </>
+                            )}
+                        </div>
+                    </Drawer>
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+}
 
 export default MobileNavbar;
