e0663851c407ab5dc6bd712ffd390b8c14141564
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-16 13:42:53 +1000

    Rename to PROXYURL

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
     app.use(
         createProxyMiddleware(["/api"], {
-            target: process.env.REACT_APP_BASEURL || "http://localhost:6001",
+            target: process.env.REACT_APP_PROXYURL || "http://localhost:6001",
             changeOrigin: true,
         })
     );
