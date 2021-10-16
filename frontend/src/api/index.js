8561ba19c06dfd855b81a320781b33122fbcd2ac
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-16 13:43:44 +1000

    Setup axios to use the proxyurl if it exists

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 export default axios.create({
-    baseURL: "api/",
+    baseURL: `${process.env.REACT_APP_PROXYURL || ""}api/`,
 });
