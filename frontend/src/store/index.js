87d0bd9ad3eed4b852c11ce69f13693da05a9bfa
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-19 13:40:24 +1000

    Add user to the root reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import AuthReducers from "./auth/Auth.reducers";
+import UserReducers from "./user/User.reducers";
 // TODO Reducers
 
 export default combineReducers({
     auth: AuthReducers,
+    user: UserReducers,
 });
