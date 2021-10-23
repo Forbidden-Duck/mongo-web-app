224861e5f0721900a214669b62f75eb9940bbdb2
Harrison Howard <harrison.howard00707@gmail.com>
2021-10-23 12:19:54 +1000

    DB actions and reducers

---
+++ frontend/src/store/db/Db.actions.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import * as dbAPI from "../../api/db";
+
+export const getAll = createAsyncThunk("db/getAll", async () => {
+    return {
+        dbs: await dbAPI.all(),
+    };
+});
+
+export const getOne = createAsyncThunk("db/getOne", async (data) => {
+    return {
+        db: await dbAPI.get(data),
+    };
+});
+
+export const createOne = createAsyncThunk("db/createOne", async (data) => {
+    return {
+        db: await dbAPI.create(data),
+    };
+});
+
+export const updateOne = createAsyncThunk("db/updateOne", async (data) => {
+    return {
+        db: await dbAPI.update(data),
+    };
+});
+
+export const deleteOne = createAsyncThunk("db/deleteOne", async (data) => {
+    await dbAPI.delete1(data);
+    return {
+        dbid: data.id,
+    };
+});
+
+export const clearError = createAction("db/clearError");
---
+++ frontend/src/store/db/Db.reducers.js
@@ -0,0 +1,113 @@
+import { createSlice } from "@reduxjs/toolkit";
+import * as dbActions from "./Db.actions";
+
+const dbSlice = createSlice({
+    name: "db",
+    initialState: {
+        isPending: false,
+        error: null,
+        dbCache: {},
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            // Clear error state
+            .addCase(dbActions.clearError, (state) => {
+                state.error = null;
+            })
+
+            // Get all pending
+            .addCase(dbActions.getAll.pending, (state) => {
+                state.isPending = true;
+            })
+            // Get all fulfilled
+            .addCase(dbActions.getAll.fulfilled, (state, action) => {
+                const { dbs } = action.payload;
+                state.isPending = false;
+                state.error = null;
+                for (const db of dbs) {
+                    state.dbCache[db._id] = db;
+                }
+            })
+            // Get all rejected
+            .addCase(dbActions.getAll.rejected, (state, action) => {
+                const { message } = action.error;
+                state.isPending = false;
+                state.error = message;
+            })
+
+            // Get db pending
+            .addCase(dbActions.getOne.pending, (state) => {
+                state.isPending = true;
+            })
+            // Get db fulfilled
+            .addCase(dbActions.getOne.fulfilled, (state, action) => {
+                const { db } = action.payload;
+                state.isPending = false;
+                state.error = null;
+                state.dbCache[db._id] = db;
+            })
+            // Get db rejected
+            .addCase(dbActions.getOne.rejected, (state, action) => {
+                const { message } = action.error;
+                state.isPending = false;
+                state.error = message;
+            })
+
+            // Create db pending
+            .addCase(dbActions.createOne.pending, (state) => {
+                state.isPending = true;
+            })
+            // Create db fulfilled
+            .addCase(dbActions.createOne.fulfilled, (state, action) => {
+                const { db } = action.payload;
+                state.isPending = false;
+                state.error = null;
+                state.dbCache[db._id] = db;
+            })
+            // Create db rejected
+            .addCase(dbActions.createOne.rejected, (state, action) => {
+                const { message } = action.error;
+                state.isPending = false;
+                state.error = message;
+            })
+
+            // Update db pending
+            .addCase(dbActions.updateOne.pending, (state) => {
+                state.isPending = true;
+            })
+            // Update db fulfilled
+            .addCase(dbActions.updateOne.fulfilled, (state, action) => {
+                const { db } = action.payload;
+                state.isPending = false;
+                state.error = null;
+                state.dbCache[db._id] = db;
+            })
+            // Update db rejected
+            .addCase(dbActions.updateOne.rejected, (state, action) => {
+                const { message } = action.error;
+                state.isPending = false;
+                state.error = message;
+            })
+
+            // Delete db pending
+            .addCase(dbActions.deleteOne.pending, (state) => {
+                state.isPending = true;
+            })
+            // Delete db fulfilled
+            .addCase(dbActions.deleteOne.fulfilled, (state, action) => {
+                const { dbid } = action.payload;
+                state.isPending = false;
+                state.error = null;
+                delete state.dbCache[dbid];
+            })
+            // Delete db rejected
+            .addCase(dbActions.deleteOne.rejected, (state, action) => {
+                const { message } = action.error;
+                state.isPending = false;
+                state.error = message;
+            });
+    },
+});
+
+export default dbSlice.reducer;
