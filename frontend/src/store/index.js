fa3c4c381a344b5a3ae913fa25a5a7b5517977c2
Harrison Howard <harrison.howard00707@gmail.com>
2021-11-08 15:07:15 +1000

    Import Db.reducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducers from "./auth/Auth.reducers";
 import UserReducers from "./user/User.reducers";
-// TODO Reducers
+import DBReducers from "./db/Db.reducers";
 
 export default combineReducers({
     auth: AuthReducers,
     user: UserReducers,
+    db: DBReducers,
 });
