708aecfc0f7675875553c077c4ef8f8bbd410075
Harrison Howard <harrison.howard00707@gmail.com>
2021-11-25 13:45:10 +1000

    Put the mongodb reducer into the root reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import AuthReducers from "./auth/Auth.reducers";
 import UserReducers from "./user/User.reducers";
 import DBReducers from "./db/Db.reducers";
+import MongoDBReducers from "./mongodb/Mongodb.reducers";
 
 export default combineReducers({
     auth: AuthReducers,
     user: UserReducers,
     db: DBReducers,
+    mongodb: MongoDBReducers,
 });
