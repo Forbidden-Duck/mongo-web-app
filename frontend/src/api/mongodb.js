db57707f58041ad4cdba5c9555a5c01f16c7cda4
Harrison Howard <harrison.howard00707@gmail.com>
2021-11-28 14:30:50 +1000

    Implement the new dbName to the frontend

--- frontend/src/api/mongodb.js
+++ frontend/src/api/mongodb.js
@@ -9,17 +9,20 @@ export const getDatabases = async (data) => {
     }
 };
 
-export const getCollections = async (data) => {
+export const getCollections = async (data, dbName) => {
     try {
-        return (await API.get("mongodb/collections", { params: { ...data } }))
-            .data;
+        return (
+            await API.get(`mongodb/collections?dbName=${dbName}`, {
+                params: { ...data },
+            })
+        ).data;
     } catch (err) {
         throw err.response.data;
     }
 };
 
-export const getDocuments = async (data, filter) => {
-    const query = [];
+export const getDocuments = async (data, dbName, filter) => {
+    const query = [`dbName=${dbName}`];
     if (filter.limit) query.push(`limit=${filter.limit}`);
     if (filter.skip) query.push(`skip=${filter.skip}`);
     try {
