c021b17a4615462a3d4073b3970688e665e3f015
Harrison Howard <harrison.howard00707@gmail.com>
2021-12-15 14:20:27 +1000

    Replace objectKeys with objectMap as objectMap hits 2 birds with 1 stone

---
+++ frontend/src/utils/objectMap.js
@@ -0,0 +1,18 @@
+/**
+ * Maps the keys and values of an object (inc. nested objects) to an array of string/value pairs
+ * @param {object} obj
+ */
+const objectMap = function (obj, _parents = []) {
+    let map = {};
+    for (const [key, value] of Object.entries(obj)) {
+        // Key is not an array but is an object
+        if (typeof value === "object" && !Array.isArray(value)) {
+            map = { ...map, ...objectMap(value, [..._parents, key]) };
+        } else {
+            map[[..._parents, key].join(".")] = value;
+        }
+    }
+    return map;
+};
+
+export default objectMap;
